4f0bb76f4330557282f0ce6e60f3415c948a2d19
Pravyn-P <pravyn.it@gmail.com>
2023-07-23 01:02:36 +0530

    msg

---
+++ terraform/locals.tf
@@ -0,0 +1,10 @@
+locals {
+    tags = {
+        Name = "timing-api-backend"
+        Environment = "DEV"
+        Terraform = "True" 
+    }
+    rds_secret_arn = data.aws_ssm_parameter.rds_secret_arn.value
+    app_alb_security_group_id = data.aws_ssm_parameter.app_alb_security_group_id.value
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+}
